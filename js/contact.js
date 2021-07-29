d7b3153c87870eb58d129b01eb2940f914911c47
Francisco Nahuel Zelaya <nahuelmassa93@gmail.com>
2021-07-29 19:34:32 -0300

    Dropdown funcionando y shadow en la imágen

--- js/contact.js
+++ js/contact.js
@@ -1,7 +1,69 @@
-function EnableDdl(){
+/* function EnableDdl(){
     var ddl = document.getElementById("DDL");
     ddl.disabled=africa.selected ? false : true;
     if(!ddl.disabled){
         ddl.focus();
     }
+} */
+
+let cargaRegion = document.getElementById("region");
+let cargaPais = document.getElementById("pais");
+
+let region = ["África", "América", "Asia", "Europa"];
+let pais = [
+  "Argentina",
+  "Brasil",
+  "Chile",
+  "Estados Unidos",
+  "México",
+  "Uruguay",
+  "Paraguay",
+  "Nigeria",
+  "Sudáfrica",
+  "Argelia",
+  "Egipto",
+  "Austria",
+  "España",
+  "Francia",
+  "Italia",
+  "Portugal",
+  "China",
+  "Japón",
+  "India",
+  "Tailandia",
+];
+
+function mostrarLugares(arreglo, lugar) {
+  let elementos = "<option selected disables>--Seleccione--</option>";
+  for (let i = 0; i < arreglo.length; i++) {
+    elementos +=
+      '<option value="' + arreglo[i] + '">' + arreglo[i] + "</option>";
+  }
+
+  lugar.innerHTML = elementos;
 }
+
+mostrarLugares(region, cargaRegion);
+function recortar(array, inicio, fin) {
+  let recortar = array.slice(inicio, fin);
+  mostrarLugares(recortar, cargaPais);
+}
+
+cargaRegion.addEventListener("change", function () {
+  let valor = cargaRegion.value;
+  console.log(valor);
+  switch (valor) {
+    case "África":
+        recortar(pais,7,11);
+      break;
+    case "América":
+        recortar(pais,0,7);
+      break;
+    case "Asia":
+        recortar(pais,16);
+      break;
+    case "Europa":
+        recortar(pais,11,16);
+      break;
+  }
+});
