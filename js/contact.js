591dfe9279ccd4f47f41dc8a2f79fb1fb06b402a
Francisco Nahuel Zelaya <nahuelmassa93@gmail.com>
2021-07-30 22:51:22 -0300

    arreglo del select de pais

--- js/contact.js
+++ js/contact.js
@@ -34,7 +34,7 @@ let pais = [
 ];
 
 function mostrarLugares(arreglo, lugar) {
-  let elementos = "<option selected disables>--Seleccione--</option>";
+  let elementos = "<option selected value='0'>--Seleccione--</option>";
   for (let i = 0; i < arreglo.length; i++) {
     elementos +=
       '<option value="' + arreglo[i] + '">' + arreglo[i] + "</option>";
@@ -49,21 +49,29 @@ function recortar(array, inicio, fin) {
   mostrarLugares(recortar, cargaPais);
 }
 
+function reset(lugar){
+  let elementos = "<option selected disabled value='0'>--Seleccione--</option>";
+  lugar.innerHTML = elementos;
+}
+
 cargaRegion.addEventListener("change", function () {
   let valor = cargaRegion.value;
   console.log(valor);
   switch (valor) {
     case "África":
-        recortar(pais,7,11);
+      recortar(pais, 7, 11);
       break;
     case "América":
-        recortar(pais,0,7);
+      recortar(pais, 0, 7);
       break;
     case "Asia":
-        recortar(pais,16);
+      recortar(pais, 16);
       break;
     case "Europa":
-        recortar(pais,11,16);
+      recortar(pais, 11, 16);
+      break;
+    case "0":
+      reset(cargaPais);
       break;
   }
 });
